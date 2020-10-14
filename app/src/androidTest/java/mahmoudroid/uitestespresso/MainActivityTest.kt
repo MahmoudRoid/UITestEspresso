5c539062bb246ed19176a98c8c6b2b51a3b8833f
Mahmoudreza Mohammadian <m.mahmoudreza1@yahoo.com>
2020-10-14 22:40:35 +0330

    fix error 'method invocation failed.min api is 26'

--- app/src/androidTest/java/mahmoudroid/uitestespresso/MainActivityTest.kt
+++ app/src/androidTest/java/mahmoudroid/uitestespresso/MainActivityTest.kt
@@ -1,13 +1,14 @@
 package mahmoudroid.uitestespresso
 
+
+
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import org.junit.Assert.*
 import org.junit.Test
+
 import org.junit.runner.RunWith
 
 
