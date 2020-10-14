017ec71e07591784a2ae983cb1abf7d3c0d6501f
Mahmoudreza Mohammadian <m.mahmoudreza1@yahoo.com>
2020-10-15 01:51:15 +0330

    add secondry activity  class test

--- app/src/androidTest/java/mahmoudroid/uitestespresso/MainActivityTest.kt
+++ app/src/androidTest/java/mahmoudroid/uitestespresso/MainActivityTest.kt
@@ -17,10 +17,27 @@ class MainActivityTest{
 
     @Test
     fun test_isActivityInView() {
-
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-
         onView(withId(R.id.main)).check(matches(isDisplayed()))
-
     }
+
+
+    @Test
+    fun test_visibility_title_nextButton() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.activity_main_title)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.button_next_activity)).check(matches(isDisplayed())) // method 1
+        onView(withId(R.id.button_next_activity))
+            .check(matches(withEffectiveVisibility(Visibility.VISIBLE))) // method 2
+    }
+
+    @Test
+    fun test_isTitleTextDisplayed(){
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.activity_main_title))
+            .check(matches(withText(R.string.text_mainactivity)))
+    }
+
+
 }
---
+++ app/src/androidTest/java/mahmoudroid/uitestespresso/SecondaryActivityTest.kt
@@ -0,0 +1,50 @@
+package mahmoudroid.uitestespresso
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class SecondaryActivityTest{
+
+    // with this line we dont need to describe activity in every method ( like MainActivityTestClass )
+    @get: Rule
+    val activityRule = ActivityScenarioRule(SecondaryActivity::class.java)
+
+    @Test
+    fun testActivity_inView() {
+
+        onView(withId(R.id.secondary))
+            .check(matches(isDisplayed()))
+
+        // Notice this does not effect the next test
+        //activityRule.scenario.moveToState(Lifecycle.State.DESTROYED)
+    }
+
+    // Visibility
+    @Test
+    fun testVisibility_title_nextButton() {
+        onView(withId(R.id.activity_secondary_title))
+            .check(matches(isDisplayed()))
+
+        onView(withId(R.id.button_back))
+            .check(matches(isDisplayed()))
+    }
+
+    // Text
+    @Test
+    fun testTitleTextDisplayed() {
+        onView(withId(R.id.activity_secondary_title))
+            .check(matches(withText(R.string.text_secondaryactivity)))
+    }
+
+
+}
