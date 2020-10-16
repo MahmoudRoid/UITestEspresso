7f1400bd24bcf8a208f19320d1f3a791a1d7e0d7
Mahmoudreza Mohammadian <m.mahmoudreza1@yahoo.com>
2020-10-16 15:24:34 +0330

    navigate between activities (2 methods)

--- app/src/androidTest/java/mahmoudroid/uitestespresso/MainActivityTest.kt
+++ app/src/androidTest/java/mahmoudroid/uitestespresso/MainActivityTest.kt
@@ -4,6 +4,8 @@ package mahmoudroid.uitestespresso
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
@@ -16,27 +18,22 @@ import org.junit.runner.RunWith
 class MainActivityTest{
 
     @Test
-    fun test_isActivityInView() {
+    fun test_navSecondaryActivity() {
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.button_next_activity)).perform(click())
+        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
+    }
+
+
+    @Test
+    fun test_backPress_tomainActivity() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.button_next_activity)).perform(click())
+        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
+
+        //onView(withId(R.id.button_back)).perform(click()) // method 1
+        pressBack() // method 2
         onView(withId(R.id.main)).check(matches(isDisplayed()))
-    }
-
-
-    @Test
-    fun test_visibility_title_nextButton() {
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-        onView(withId(R.id.activity_main_title)).check(matches(isDisplayed()))
-
-        onView(withId(R.id.button_next_activity)).check(matches(isDisplayed())) // method 1
-        onView(withId(R.id.button_next_activity))
-            .check(matches(withEffectiveVisibility(Visibility.VISIBLE))) // method 2
-    }
-
-    @Test
-    fun test_isTitleTextDisplayed(){
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-        onView(withId(R.id.activity_main_title))
-            .check(matches(withText(R.string.text_mainactivity)))
     }
 
 
