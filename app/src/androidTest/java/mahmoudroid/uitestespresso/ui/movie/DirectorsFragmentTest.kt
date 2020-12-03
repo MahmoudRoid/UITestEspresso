b99789cded99f658db7616c2ccddbf03337da633
Mahmoudroid <m.mahmoudreza1@gmail.com>
2020-12-03 22:45:50 +0330

    directors names

--- app/src/androidTest/java/mahmoudroid/uitestespresso/ui/movie/DirectorsFragmentTest.kt
+++ app/src/androidTest/java/mahmoudroid/uitestespresso/ui/movie/DirectorsFragmentTest.kt
@@ -8,31 +8,32 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import kotlinx.android.synthetic.main.fragment_movie_detail.view.*
+import androidx.test.runner.AndroidJUnitRunner
+import kotlinx.android.synthetic.main.fragment_directors.view.*
 import mahmoudroid.uitestespresso.R
-import mahmoudroid.uitestespresso.data.DummyMovies.THE_RUNDOWN
+import mahmoudroid.uitestespresso.data.DummyMovies.INFINITY_WAR
+import mahmoudroid.uitestespresso.factory.MovieFragmentFactory
+import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
-class eMovieDetailFragmentTest{
+class DirectorsFragmentTest{
+
 
     @Test
-    fun test_isMovieDataVisible() {
-
-        val movie = THE_RUNDOWN
-        val fragmentFactory = FragmentFactory()
+    fun test_isDircrorsListVisible() {
+        val movie = INFINITY_WAR
+        val fragmentFactory = MovieFragmentFactory()
         val bundle = Bundle()
-        bundle.putInt("movie_id",movie.id)
+        bundle.putStringArrayList("args_directors",movie.directors)
 
-        val scenario = launchFragmentInContainer<MovieDetailFragment>(
+        val scenario = launchFragmentInContainer<DirectorsFragment>(
             factory = fragmentFactory,
             fragmentArgs = bundle
         )
 
-        onView(withId(R.id.movie_title)).check(matches(withText(movie.title)))
-        onView(withId(R.id.movie_description)).check(matches(withText(movie.description)))
-
+        onView(withId(R.id.directors_text)).check(matches(withText(movie.directors.toString())))
 
     }
 }
