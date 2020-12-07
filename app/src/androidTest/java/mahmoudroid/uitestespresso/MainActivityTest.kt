3317ad9d5a895eca6dd339049172efd1898f29e4
Mahmoudreza Mohammadian <m.mahmoudreza1@yahoo.com>
2020-12-07 18:34:10 +0330

    test dialog

--- app/src/androidTest/java/mahmoudroid/uitestespresso/MainActivityTest.kt
+++ app/src/androidTest/java/mahmoudroid/uitestespresso/MainActivityTest.kt
@@ -1,66 +1,45 @@
 package mahmoudroid.uitestespresso
 
-
-
-import android.app.Activity
-import android.app.Activity.RESULT_OK
-import android.app.Instrumentation
-import android.content.ContentResolver
-import android.content.Intent
-import android.content.res.Resources
-import android.graphics.BitmapFactory
-import android.net.Uri
-import android.os.Bundle
-import android.provider.MediaStore
+import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.intent.Intents.intended
-import androidx.test.espresso.intent.Intents.intending
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasData
-import androidx.test.espresso.intent.rule.IntentsTestRule
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import androidx.test.platform.app.InstrumentationRegistry
-import mahmoudroid.uitestespresso.ImageViewHasDrawableMatcher.hasDrawable
-import org.hamcrest.CoreMatchers.not
-import org.hamcrest.Matcher
-import org.hamcrest.Matchers.allOf
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class MainActivityTest{
 
-    @get:Rule
-    val intentsTestRule = IntentsTestRule(MainActivity::class.java)
 
     @Test
-    fun test_ValidateAndStubIntent() {
+    fun test_showDialog_captureNameInput() {
         // GIVEN
-        val activityResult = createImageCaptureActivityResultStub()
-        val expectedIntent: Matcher<Intent> = hasAction(MediaStore.ACTION_IMAGE_CAPTURE)
-        intending(expectedIntent).respondWith(activityResult)
+        val activityScenatio = ActivityScenario.launch(MainActivity::class.java)
+        val text = "Hello"
 
-        // Execute and Verify
-        onView(withId(R.id.image)).check(matches(not(hasDrawable()))) // custom matcher
-        onView(withId(R.id.button_launch_camera)).perform(click())
-        intended(expectedIntent) // check intent is executed or not
-        onView(withId(R.id.image)).check(matches(hasDrawable())) // check imageview has any drawable
+        onView(withId(R.id.text_name)).check(matches(withText("")))
+        onView(withId(R.id.button_launch_dialog)).perform(click())
+
+        // check for show dialog
+        onView(withText(R.string.text_enter_name)).check(matches(isDisplayed()))
+
+        // check for click ok with empty edt ==> should not disappear
+        onView(withText(R.string.text_ok)).perform(click())
+        onView(withText(R.string.text_enter_name)).check(matches(isDisplayed()))
+
+        // type data into InputLayout and click ==> dialog should disappear
+        onView(withId(R.id.md_input_message)).perform(typeText(text))
+        onView(withText(R.string.text_ok)).perform(click())
+        onView(withText(R.string.text_enter_name)).check((doesNotExist()))
+
+
+        // check text view text
+        onView(withId(R.id.text_name)).check(matches(withText(text)))
+
     }
 
-    private fun createImageCaptureActivityResultStub(): Instrumentation.ActivityResult? {
-        val bundle = Bundle()
-        bundle.putParcelable(
-            KEY_IMAGE_DATA, BitmapFactory.decodeResource(
-                intentsTestRule.getActivity().getResources(),
-                R.drawable.ic_launcher_background
-            )
-        )
-        val resultData = Intent()
-        resultData.putExtras(bundle)
-        return Instrumentation.ActivityResult(Activity.RESULT_OK, resultData)
-    }
 }
--- app/src/main/java/mahmoudroid/uitestespresso/MainActivity.kt
+++ app/src/main/java/mahmoudroid/uitestespresso/MainActivity.kt
@@ -1,19 +1,13 @@
  package mahmoudroid.uitestespresso
 
-import android.app.Activity
-import android.content.Intent
-import android.graphics.Bitmap
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
-import android.util.Log
-import com.bumptech.glide.Glide
+import androidx.appcompat.app.AppCompatActivity
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.input.input
 import kotlinx.android.synthetic.main.activity_main.*
 
- const val REQUEST_IMAGE_CAPTURE = 1234
- const val KEY_IMAGE_DATA = "data"
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
 
     private val TAG: String = "AppDebug"
 
@@ -21,33 +15,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button_launch_camera.setOnClickListener {
-            dispatchCameraIntent()
+        button_launch_dialog.setOnClickListener {
+            showDialog()
         }
+
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(resultCode == Activity.RESULT_OK){
-            Log.d(TAG, "RESULT_OK")
-            when(requestCode){
-
-                REQUEST_IMAGE_CAPTURE -> {
-                    Log.d(TAG, "REQUEST_IMAGE_CAPTURE detected.")
-                    data?.extras.let{ extras ->
-                        if (extras == null || !extras.containsKey(KEY_IMAGE_DATA)) {
-                            return
-                        }
-                        val imageBitmap = extras[KEY_IMAGE_DATA] as Bitmap?
-                        image.setImageBitmap(imageBitmap)
-                    }
+    private fun showDialog(){
+        MaterialDialog(this)
+            .show {
+                input (
+                    waitForPositiveButton = true,
+                    allowEmpty = false
+                ){ dialog, name ->
+                    setNameToTextView(name.toString())
                 }
+
+
+                title(R.string.text_enter_name)
+                positiveButton(R.string.text_ok)
             }
-        }
     }
 
-    private fun dispatchCameraIntent() {
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+    private fun setNameToTextView(name: String){
+        text_name.text = name
     }
+
 }
